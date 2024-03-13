43ef4081d7da7bf695e0813584785c24478b01fe
lucarl07 <junitinho2007@gmail.com>
2024-03-13 11:20:58 -0300

    Tornar o exercício 04 funcional, mas sem limite de chutes

--- exercicios/ex004/js/app.js
+++ exercicios/ex004/js/app.js
@@ -3,44 +3,55 @@
  * Iniciado em 13/03/2024.
 */
 
-// Inputs:
+// Entrada de dados:
 const inNumero = document.querySelector('#inNumero'),
 btnApostar = document.querySelector('#btnApostar'),
 btnJogar = document.querySelector('#btnJogar');
 
-// Outputs:
+// Saída de dados:
 const outErros = document.querySelector('#outErros'),
 outChances = document.querySelector('#outChances'),
 outDica = document.querySelector('#outDica');
 
-const gerarNumeroAleatorio = (max) => {
-    return Math.floor(Math.random() * max) + 1
-}
+// Gerar número inteiro aleatório:
+const gerarNumeroAleatorio = (max) => Math.floor(Math.random() * max) + 1;
+const numSecreto = gerarNumeroAleatorio(100)
 
-const numSorteado = gerarNumeroAleatorio(100)
-
-let numPossivel = parseInt(inNumero.value);
+// Valores no output:
 let erros = [], chances = parseInt(outChances.innerHTML);
 
 const adivinharNumero = () => {
-    if (numSorteado < numPossivel) {
-        erros.push(numPossivel)
-        chances--;
-        
-        outErros.innerHTML = erros;
-        outChances.innerHTML = chances;
-        outDica.innerHTML = `É um número menor que ${numPossivel}`
-    } else if (numSorteado > numPossivel) {
-        erros.push(numPossivel)
-        chances--;
-        
-        outErros.innerHTML = erros;
-        outChances.innerHTML = chances;
-        outDica.innerHTML = `É um número maior que ${numPossivel}`
+    // Adicionar string de erro formatado e o número possível
+    let formatarErr = '';
+    let numPossivel = parseInt(inNumero.value);
+
+    // Comparar o número secreto com o chute
+    if (numSecreto !== numPossivel) {
+        validarNumero(formatarErr, numPossivel);
     } else {
-        alert('Parabéns, você acertou!')
-        outDica.innerHTML = `É o número ${numPossivel}!`
+        alert(`Parabéns, você acertou! \nO número secreto é ${numPossivel}`)
+        outDica.innerHTML = `É o número ${numSecreto}!`
+        inNumero.value = '';
     }
+}
+
+const validarNumero = (listaErros, chute) => {
+    // Adicionar número errado na array "erros"
+    erros.push(chute)
+    listaErros = `(${erros.join(', ')})`
+
+    // Expor valores no HTML
+    chances--;
+    outErros.innerHTML = listaErros;
+    outChances.innerHTML = chances;
+
+    // Expor a dica e limpar o campo de entrada
+    if (numSecreto < chute) {
+        outDica.innerHTML = `É um número menor que ${chute}`
+    } else {
+        outDica.innerHTML = `É um número maior que ${chute}`
+    }
+    inNumero.value = '';
 }
 
 btnApostar.addEventListener('click', adivinharNumero)
