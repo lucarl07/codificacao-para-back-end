21933f4953ad9b8e7a432428a20ea12d1aa19ed1
lucarl07 <junitinho2007@gmail.com>
2024-03-25 08:29:30 -0300

    Finalizar a primeira atividade de ./objetos/

---
+++ objetos/atv01.js
@@ -0,0 +1,28 @@
+/** ATIVIDADE 01
+ * Proposta: Crie uma função que aceite o ID da linha como entrada e retorne os horários de partida e chegada para essa linha.
+ */
+
+// Chamando os dados de onibus.json:
+const data = require('./onibus.json')
+
+// Declarando o ID da linha de ônibus a ser pesquisada:
+const busRouteId = 2;
+
+// Função para obter os horários de uma linha de ônibus:
+const getRouteSchedule = (id) => {
+    const busRoute = data.linhas.find(route => route.id === id);
+    
+    // Caso o ID inserido seja inválido:
+    if (busRoute === undefined) {
+        throw new Error('ID erroneamente digitado, ou linha de ônibus inexistente.')
+    }
+
+    const busSchedule = busRoute.horarios,
+    arrival = busSchedule.map(obj => obj.chegada).join(' | '),
+    departure = busSchedule.map(obj => obj.saida).join(' | ');
+
+    console.log(`HORÁRIOS DE CHEGADA: \n${arrival} \nHORÁRIOS DE SAÍDA: \n${departure}`)    
+}
+
+// Chamando a função mencionada acima:
+getRouteSchedule(busRouteId);
