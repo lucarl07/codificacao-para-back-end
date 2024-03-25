96bef9afd6510d23a410a7b3da1138ff380af5e4
lucarl07 <junitinho2007@gmail.com>
2024-03-25 09:44:52 -0300

    Finalizar a 4ª atividade de objetos

---
+++ objetos/atv04.js
@@ -0,0 +1,32 @@
+/** ATIVIDADE 04 - Filtrar ônibus por status 
+ * Proposta: Implemente uma função que aceite um status como entrada (por exemplo, "em operação" ou "em manutenção") e retorne uma lista de ônibus com esse status.
+*/
+
+// Chamando os dados de onibus.json:
+const data = require('./onibus.json')
+
+// Declarando o status dos ônibus a serem pesquisados:
+const busStatus = "em operação";
+
+// Função para obter uma lista de ônibus por status operacional:
+const getBusesByStatus = (status) => {
+    const buses = data.onibus.filter(bus => bus.status === status);
+    
+    if (buses.length === 0) {
+        console.log('Não há ônibus no status operacional especificado.')
+        return;
+    }
+
+    let aboutBuses = `===== ÔNIBUS ${status.toUpperCase()} =====\n`;
+    let count = 1;
+
+    buses.forEach(bus => {
+        aboutBuses += `${count}. Veículo ${bus.id} \n\tModelo: ${bus.modelo} \n\tPlaca: ${bus.placa} \n\tLinha: ${bus.linha} \n`
+        count++;
+    })
+
+    console.log(aboutBuses)
+}
+
+// Chamando a função:
+getBusesByStatus(busStatus)
