b0cf027431558943153936a2b2b85d100584ec98
lucarl07 <junitinho2007@gmail.com>
2024-03-25 09:14:17 -0300

    Finalizar a 3ª atividade avaliativa

---
+++ objetos/atv03.js
@@ -0,0 +1,30 @@
+/** ATIVIDADE 03
+ * Proposta: Escreva uma função que aceite o ID da linha como entrada e retorne todas as paradas associadas a essa linha, ordenadas pela ordem de parada.
+ */
+
+// Chamando os dados de onibus.json:
+const data = require('./onibus.json')
+
+// Declarando o ID da linha de ônibus a ser pesquisada:
+const busRouteId = 2;
+
+// Função para obter todas as paradas da linha:
+const getBusStops = (id) => {
+    const busRoute = data.linhas.find(route => route.id === id);
+
+    // Caso o ID inserido seja inválido:
+    if (busRoute === undefined) {
+        throw new Error('ID erroneamente digitado, ou linha de ônibus inexistente.')
+    }
+
+    const busStops = busRoute.paradas;
+    let aboutStops = `===== LINHA ${id} =====\n`;
+
+    busStops.forEach((stop, i) => {
+        aboutStops += `${stop.ordem}. ${stop.nome} (${stop.id}) \n\tX: ${stop.latitude} \n\tY: ${stop.longitude}\n`
+    })
+
+    console.log(aboutStops)
+}
+
+getBusStops(busRouteId);
