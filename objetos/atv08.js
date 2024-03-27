cd4434832da47e1780ca51a15b649c72ee3ba3de
lucarl07 <junitinho2007@gmail.com>
2024-03-27 08:47:19 -0300

    Adicionar texto explicativo em ./objetos/atv08.js

---
+++ objetos/atv08.js
@@ -0,0 +1,20 @@
+/** ATIVIDADE 08 - Atualizar informações do ônibus 
+ * Proposta: Crie uma função que aceite o ID de um ônibus e novas informações (como status, motorista, etc.) e atualize essas informações na base de dados.
+*/
+
+// Para a realização dessa atividade, será necessário 
+
+// o uso de determinados conhecimentos em funções a-
+
+// ssíncronas no Node.js. Por este motivo, a ativi-
+
+// dade não poderá ser finalizada no momento. Então,
+
+// fiquem com isso:
+
+const helloWorld = (text) => {
+    let i = 'sim'
+    console.log(text);
+}
+
+helloWorld(`console.log('Hello world!')`);
