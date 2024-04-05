43b6efa23fb172272c3741f31c076f432339722e
lucarl07 <junitinho2007@gmail.com>
2024-04-05 08:14:10 -0300

    Aprender sobre o uso e importância do try-catch e da classe Error

---
+++ nodeJS/FUNDAMENTOS/errors/throw.js
@@ -0,0 +1,15 @@
+/* DEMONSTRAÇÃO: */
+// // Bloco de código para tratar instruções async
+// try {
+//     // Instrução de espera para acontecer
+// } catch (error) {
+//     console.log(error)
+// }
+
+const b = 10
+
+if (!Number.isInteger(b)) {
+    throw new Error('O valor de B não é um valor inteiro!')
+}
+
+console.log('Resto dos códigos.')
